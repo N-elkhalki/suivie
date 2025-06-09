9fe30659afaf21de83e5f241965ab6ccb6bff576
N-elkhalki <108853201+N-elkhalki@users.noreply.github.com>
2025-06-09 09:52:58 +0100

    Update db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "91.216.107.162";
-$username = "osour2189855";
-$password = "kU8!fVfVK1TPq8W";
-$dbname = "osour2189855";
+$servername = "localhost";
+$username = "osour2185";
+$password = "kU8!fV145TPq8W";
+$dbname = "osour2185";
 
 $connection = new mysqli($servername, $username, $password, $dbname);
 if ($connection->connect_error) {
